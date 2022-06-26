43a7bd2318d0271b5aae1fe2979cf076d6322177
zhanghongwei06 <zhanghongwei06@58.com>
2022-06-26 12:10:31 +0800

    测试一下reset

--- src/main/java/com/zhw/git/controller/SystemController.java
+++ src/main/java/com/zhw/git/controller/SystemController.java
@@ -3,6 +3,6 @@ package com.zhw.git.controller;
 public class SystemController {
 
     public void index(){
-        System.out.println("controller-index-5");
+        System.out.println("controller-index-6");
     }
 }
