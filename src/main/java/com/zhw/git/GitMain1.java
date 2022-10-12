07bb07753185dd68010bd77f310fd662c22ab117
yongheng <zhw2008game@163.com>
2022-10-12 17:22:08 +0800

    3

---
+++ src/main/java/com/zhw/git/GitMain1.java
@@ -0,0 +1,10 @@
+package com.zhw.git;
+
+public class GitMain1 {
+
+    public static void main(String[] args) {
+        System.out.println(1);
+        System.out.println(2);
+        System.out.println(3);
+    }
+}
