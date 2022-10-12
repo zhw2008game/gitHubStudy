9d38bcca12ffd0234f85a725e4b1a768c16f613b
yongheng <zhw2008game@163.com>
2022-10-12 15:13:58 +0800

    11111

--- src/main/java/com/zhw/git/gitMain.java
+++ src/main/java/com/zhw/git/gitMain.java
@@ -2,10 +2,6 @@ package com.zhw.git;
 
 public class gitMain {
     public static void main(String[] args) {
-        System.out.println(1);
-        System.out.println(2);
-        System.out.println(3);
-        System.out.println(4);
-        System.out.println(5);
+
     }
 }
